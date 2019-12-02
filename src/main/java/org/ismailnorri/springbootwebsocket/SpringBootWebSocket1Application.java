6b178e8470e93365521e61f6900787575d5068c8
Ismail NORRI <ismail.norri.24@gmail.com>
2019-12-02 21:07:22 +0000

    Application de Chat avec Spring Boot et Web Socket

---
+++ src/main/java/org/ismailnorri/springbootwebsocket/SpringBootWebSocket1Application.java
@@ -0,0 +1,13 @@
+package org.ismailnorri.springbootwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocket1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocket1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/ismailnorri/springbootwebsocket/SpringBootWebSocket1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ismailnorri.springbootwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSocket1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
